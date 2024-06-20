a77b63ff51642ab10eb25e55c0c8f043606177b4
mastewaltadese <mastewaltadese7@gmail.com>
2024-06-20 08:46:40 -0700

    calculator

---
+++ basic_operations.py
@@ -0,0 +1,11 @@
+#!/bin/bash
+number1 = 10
+number2 = 5
+sum = 10 + 5
+subtrction = number1 - number2
+multy =  number1 * number2
+print("Addition of " + str(number1) + " and "+ str(number2) +" is "+ str(sum))
+print("subtraction of " + str(number1) + " and " + str(number2) + " is " + str(subtrction))
+print("Multiplication of {} and {} is {}".format(number1,number2,multy))
+
+
