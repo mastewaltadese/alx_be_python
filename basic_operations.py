4d55d9fa5fb1da6ab97fb621c665e506a682d408
mastewaltadese <mastewaltadese7@gmail.com>
2024-06-21 00:59:10 -0700

    this is calculator

--- basic_operations.py
+++ basic_operations.py
@@ -3,9 +3,9 @@ number1 = 10
 number2 = 5
 sum = 10 + 5
 subtrction = number1 - number2
-multy =  number1 * number2
+multi =  number1 * number2
 print("Addition of " + str(number1) + " and "+ str(number2) +" is "+ str(sum))
 print("subtraction of " + str(number1) + " and " + str(number2) + " is " + str(subtrction))
-print("Multiplication of {} and {} is {}".format(number1,number2,multy))
+print("Multiplication of {} and {} is {}".format(number1,number2,multi))
 
 
