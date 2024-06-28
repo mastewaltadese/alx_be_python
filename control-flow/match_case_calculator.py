855589d4b7e457940e95c8043fa74d07fec007bd
mastewaltadese <mastewaltadese7@gmail.com>
2024-06-28 06:00:48 -0700

    mach calculator

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,19 @@
+#!/bin/bash
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the scond number:"))
+operation = (input("hoose the operation (+, -, *, /):"))
+result = 0
+match operation:
+  case "+":
+    result= num1 + num2
+  case "-":
+    result= num1 - num2
+  case "*":
+    result= num1 * num2
+  case "/":
+    if num2 == 0:
+        print(f"{num1} can't be divided by 0")
+    elif num2 != 0:
+        result = num1 / num2
+print(f"result is {result}.")
+
